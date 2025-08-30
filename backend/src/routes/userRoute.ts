8ef2c144ea22f77c3826c70b161b9af0423cb61c
92Mohammad <m.e89571828@gmail.com>
2025-08-30 17:21:01 +0530

    added profile route to serve the user profile detail

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -4,13 +4,17 @@ import { User } from "../models/User";
 
 const router = Router();
 
-// Example protected route
+
 router.get("/profile", authMiddleware, async (req: Request, res: Response) => {
 	const { userId } = req;
 	console.log('userId: ', userId);
 
-    const user = await User.findById(userId).select('username email');;
+    const user = await User.findById(userId).select('username email createdAt');
 	console.log('this is user: ', user)
+
+	if (!user) {
+		return res.status(404).json({ message: "User not found" });
+	}
 
 	res.json({
 		message: "Welcome to your profile",
