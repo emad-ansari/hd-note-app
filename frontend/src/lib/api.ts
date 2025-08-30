844fc7b9b61674cf9889f8b212fca011e4431bd1
92Mohammad <m.e89571828@gmail.com>
2025-08-30 21:42:12 +0530

    added host backend api url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,4 +1,10 @@
-const API_BASE_URL = 'http://localhost:4000/api';
+let API_BASE_URL = 'http://localhost:4000/api';
+
+if (import.meta.env.VITE_ENV === "development") {
+	API_BASE_URL = import.meta.env.VITE_LOCALHOST_URL;
+} else {
+	API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+}
 
 interface ApiResponse<T = any> {
   success: boolean;
